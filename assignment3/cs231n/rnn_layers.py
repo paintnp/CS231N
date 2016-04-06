c943e73acf78095e87f742c3d2ec400d193dc924
Labhesh Patel <labhesh@gmail.com>
2016-04-05 21:56:16 -0700

    optimized word_embedding_forward

--- assignment3/cs231n/rnn_layers.py
+++ assignment3/cs231n/rnn_layers.py
@@ -185,19 +185,6 @@ def word_embedding_forward(x, W):
   out = np.zeros((N,T,D))
   x_flattened = x.flatten()
   out = W[x_flattened, :].reshape(x.shape[0], x.shape[1], W.shape[1])
-
-  # for n in range(0,N):
-  #     for t in range(0,T):
-  #         out[n,t,:] = W[x[n,t]]
-  ##############################################################################
-  # TODO: Implement the forward pass for word embeddings.                      #
-  #                                                                            #
-  # HINT: This should be very simple.                                          #
-  ##############################################################################
-  pass
-  ##############################################################################
-  #                               END OF YOUR CODE                             #
-  ##############################################################################
   cache = x,W
   return out, cache
 
